fca9b57630e8e6fc1b381bd33217700e346207d5
Amr Hossam <amrhossam97@gmail.com>
2019-12-13 19:53:35 +0200

    remove number of levels

--- NodeClass.py
+++ NodeClass.py
@@ -1,5 +1,4 @@
 class Node:
-    total_number_of_levels = 0
     def __init__(self, parent, is_child, key, level, is_rectangle, number_of_children = 0, posX = 0):
         self.parent = parent
         self.is_child = is_child
@@ -8,5 +7,3 @@ class Node:
         self.number_of_children = 0
         self.posX = 0
         self.is_rectangle = is_rectangle
-        if(level > Node.total_number_of_levels):
-            Node.total_number_of_levels = level
